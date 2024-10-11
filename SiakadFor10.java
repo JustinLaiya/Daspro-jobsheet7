748bd2d615846196fa1ae756fcee8b9acb45e077
JustinLaiya <laiyajustin@gmail.com>
2024-10-11 20:48:45 +0700

    modip mobil

--- SiakadFor10.java
+++ SiakadFor10.java
@@ -4,6 +4,7 @@ public class SiakadFor10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
         for (int i = 1; i <= 10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -13,8 +14,16 @@ public class SiakadFor10 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >=60){
+                jumlahLulus++;
+            }else{
+                jumlahTidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jumlahTidakLulus);
+        sc.close();
     }
 }
